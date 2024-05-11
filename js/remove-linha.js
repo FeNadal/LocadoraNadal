5dd5f1c82e54a70d89fde5beb418a43494720a48
FeNadal <145722318+FeNadal@users.noreply.github.com>
2024-05-10 21:52:07 -0300

    Add files via upload

---
+++ js/remove-linha.js
@@ -0,0 +1,9 @@
+var tabela = document.querySelector("#remove-table");
+
+tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+})
