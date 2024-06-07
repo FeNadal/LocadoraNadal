a03dcd42b9fc9b1d6df1e11afb78680e060a504c
FeNadal <145722318+FeNadal@users.noreply.github.com>
2024-06-07 18:50:11 -0300

    Add files via upload

---
+++ js/api-nova.js
@@ -0,0 +1,34 @@
+const apiKey = '252d0215';
+const frmPesquisa = document.querySelector(".forma");
+
+
+frmPesquisa.onsubmit = (ev) => {
+    ev.preventDefault();
+    
+    const pesquisa= ev.target.pesquisa.value;
+
+    if(pesquisa == ""){
+        alert("Preencha o campo!")
+        return;
+    }
+
+    fetch(`https://www.omdbapi.com/?s=${pesquisa}&apikey=${apiKey}`)
+        .then(result => result.json())
+        .then(json => carregaLista(json))
+}
+
+const carregaLista =(json) => {
+    const lista = document.querySelector("div.lista2");
+    lista.innerHTML="";
+
+    json.Search.forEach(element => {
+        console.log(element);
+
+        let item = document.createElement("div");
+        item.classList.add("item");
+
+        item.innerHTML = `<img class="imagem2" src="${element.Poster}" /><h2>${element.Title}</h2>`
+
+        lista.appendChild(item);
+    });
+}
